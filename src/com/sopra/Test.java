3088e8e8b40325932c69b8087a957f2fe49e47f2
KAVITI Vandana <vandana.kaviti@soprasteria.com>
2022-09-22 11:01:09 +0530

    modified

--- src/com/sopra/Test.java
+++ src/com/sopra/Test.java
@@ -3,10 +3,11 @@ package com.sopra;
 public class Test {
 	public static void test1() {
 		System.out.println("Test 1 done");
+		System.out.println("Changes made in test 1 method");
 	}
 	
 	public static void test2() {
-		System.out.println("Test 2 done");
+		System.out.println("Test 2 changed");
 	}
 	
 	public static void test3() {
