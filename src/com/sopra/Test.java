eedf34daa408a6d0bf31c61f6514c0c4a7b73db0
KAVITI Vandana <vandana.kaviti@soprasteria.com>
2022-09-22 10:47:22 +0530

    first commit

---
+++ src/com/sopra/Test.java
@@ -0,0 +1,15 @@
+package com.sopra;
+
+public class Test {
+	public static void test1() {
+		System.out.println("Test 1 done");
+	}
+	
+	public static void test2() {
+		System.out.println("Test 2 done");
+	}
+	
+	public static void test3() {
+		System.out.println("Test 3 done");
+	}
+}
